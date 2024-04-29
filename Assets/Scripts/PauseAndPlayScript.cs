ec02de3311e9bac109de36a0ecf9f4bb983a568a
Shashank-N-K <167828089+Shashank-N-K@users.noreply.github.com>
2024-04-29 04:08:13 -0500

    UI Almost done

---
+++ Assets/Scripts/PauseAndPlayScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseAndPlayScript : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject playMenu;
+    private bool isPaused = false;
+
+    void Awake()
+    {
+        pauseMenu.SetActive(false); 
+        playMenu.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(isPaused);
+            playMenu.SetActive(!isPaused);
+
+            Cursor.lockState = isPaused ? CursorLockMode.None : CursorLockMode.Locked;
+            Cursor.visible = isPaused;
+            Time.timeScale = isPaused ? 0.0f : 1.0f;
+
+            isPaused = !isPaused;
+        }
+    }
+}
