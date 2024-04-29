0400aae29925158602d408c22498a8d2aa2bf99f
Shashank-N-K <167828089+Shashank-N-K@users.noreply.github.com>
2024-04-28 21:35:57 -0500

    completed animation

--- Assets/Scripts/AnimationStateControl.cs
+++ Assets/Scripts/AnimationStateControl.cs
@@ -10,6 +10,11 @@ public class AnimationStateControl : MonoBehaviour
     private float velocityZ = 0.0f;
 
     public float acceleration = 2.0f;
+    public float deceleration = 3.0f;
+
+    public float maxWalkSpeed = 1.0f;
+    public float maxRunSpeed = 2.5f;
+
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -23,21 +28,69 @@ public class AnimationStateControl : MonoBehaviour
         bool rightPressed = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
         bool backPressed = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
         bool runPressed = Input.GetKey(KeyCode.LeftShift);
+        bool jumpPressed = Input.GetKey(KeyCode.Space);
+        //
+        float maxVelocity = runPressed ? maxRunSpeed : maxWalkSpeed;
 
-        if(forwardPressed)
+        if(forwardPressed && velocityZ < maxVelocity)
         {
             velocityZ += Time.deltaTime * acceleration;
-
         }
-
-        if(leftPressed)
+    
+        if(backPressed && velocityZ > -maxVelocity)
+        {
+            velocityZ -= Time.deltaTime * acceleration;
+        }
+        
+        if(leftPressed && velocityX > -maxVelocity)
         {
             velocityX -= Time.deltaTime * acceleration;
         }
-
-        if(rightPressed)
+        
+        if (rightPressed && velocityX < maxVelocity)
         {
             velocityX += Time.deltaTime * acceleration;
         }
+        //
+
+
+        if(!forwardPressed && velocityZ > 0.0f)
+                velocityZ -= Time.deltaTime * acceleration;
+        else if (!backPressed && velocityZ < 0.0f)
+                velocityZ += Time.deltaTime * acceleration;;
+        if(!forwardPressed && !backPressed && velocityZ !=0 && (velocityZ > -0.05f && velocityZ < 0.05f))
+                velocityZ = 0.0f;
+
+        if (!leftPressed && velocityX < 0.0f)
+            velocityX += Time.deltaTime * deceleration;
+        else if(!rightPressed && velocityX > 0.0f)
+            velocityX -= Time.deltaTime * deceleration;
+        if (!leftPressed && !rightPressed && velocityX != 0.0f && (velocityX > -0.05f && velocityX < 0.05f))
+            velocityX = 0.0f;
+
+        if (runPressed && forwardPressed && velocityZ > maxVelocity)
+            velocityZ = maxVelocity;
+        else if (runPressed && backPressed && velocityZ < -maxVelocity)
+            velocityZ = -maxVelocity;
+        else if (forwardPressed && velocityZ > maxVelocity)
+        { 
+            velocityZ -= Time.deltaTime * deceleration;
+            if (velocityZ > maxVelocity + 0.05f)
+                velocityZ = maxVelocity;
+        }
+        else if (backPressed && velocityZ < -maxVelocity)
+        { 
+            velocityZ += Time.deltaTime * deceleration; 
+            if (velocityZ < -maxVelocity - 0.05f)
+                velocityZ = -maxVelocity;
+        }
+        else if (forwardPressed && velocityZ < maxVelocity && velocityZ > (maxVelocity - 0.05f))
+            velocityZ = maxVelocity;
+        else if (backPressed && velocityZ > -maxVelocity && velocityZ < (0.05f - maxVelocity))
+            velocityZ = -maxVelocity;
+
+
+        animator.SetFloat("Velocity Z", velocityZ);
+        animator.SetFloat("Velocity X", velocityX);
     }
 }
